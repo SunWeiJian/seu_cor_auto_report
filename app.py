86d888a6211ef7be7da5ab1bb08655225c505552
yuan fang <494872172@qq.com>
2020-02-27 19:54:25 +0800

    使用selenium模拟浏览器

---
+++ app.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+
+
+opt = webdriver.ChromeOptions()  # 创建浏览器
+opt.headless = True  # 无窗口模式
+driver = webdriver.Chrome(options=opt)  # 创建浏览器对象
+driver.get('https://xgbxscwx.seu.edu.cn/#/')  # 打开网页
+time.sleep(3)
+
+driver.find_element_by_xpath("//input[@id='username']").send_keys("213163540")
+driver.find_element_by_xpath("//input[@id='password']").send_keys("sdzjfy121")
+driver.find_element_by_xpath("//button[@type='submit']").click()
+time.sleep(2)
+
+print(driver.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/h2").text)
+driver.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]").click()
+# todo: clike()
+print('上报成功！')
