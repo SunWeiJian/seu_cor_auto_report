aa0d2fc9f5b34a685df160b7c26558de05ba4f6b
yuan fang <494872172@qq.com>
2020-02-27 20:18:49 +0800

    add readme.

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from selenium import webdriver
 import time
 
+from selenium import webdriver
 
 opt = webdriver.ChromeOptions()  # 创建浏览器
-opt.headless = True  # 无窗口模式
+# opt.headless = True  # 无窗口模式 可注释此行查看chrome运行情况
 driver = webdriver.Chrome(options=opt)  # 创建浏览器对象
 driver.get('https://xgbxscwx.seu.edu.cn/#/')  # 打开网页
 time.sleep(3)
 
-driver.find_element_by_xpath("//input[@id='username']").send_keys("学号")
-driver.find_element_by_xpath("//input[@id='password']").send_keys("密码")
+driver.find_element_by_xpath("//input[@id='username']").send_keys("账号")  # 填入你的一卡通号
+driver.find_element_by_xpath("//input[@id='password']").send_keys("密码")  # 填入你的密码
 driver.find_element_by_xpath("//button[@type='submit']").click()
 time.sleep(2)
 
 print(driver.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/h2").text)
 driver.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]").click()
-# todo: clike()
+# todo: clike()  仍需模拟最后一次点击
 print('上报成功！')
