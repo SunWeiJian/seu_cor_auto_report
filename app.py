c5980bcedb252b3f1dd4ecfc9ef3eda988f2492f
SunWeiJian <weijiansun@outlook.com>
2020-03-29 14:16:02 +0800

    update app.py

--- app.py
+++ app.py
@@ -23,23 +23,24 @@ options.add_argument("--headless");
 browser = webdriver.Chrome(options=options)
 browser.set_window_size(1200, 2000)
 
-browser.get('https://xgbxscwx.seu.edu.cn/#/')  # 打开网页
+browser.get('https://newids.seu.edu.cn/authserver/login?service=http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/*default/index.do')  # 打开网页
 time.sleep(3)
 print("1")
-browser.find_element_by_xpath("//input[@id='username']").send_keys(user)  # 填入你的一卡通号
-browser.find_element_by_xpath("//input[@id='password']").send_keys(pwd)  # 填入你的密码
-browser.find_element_by_xpath("//button[@type='submit']").click()
-time.sleep(2)
-print("2")
-print(browser.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/h2").text)
-browser.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/div/div/div[1]").click()
-time.sleep(2)
+browser.find_element_by_xpath('//*[@id="username"]').send_keys(user)  # 填入你的一卡通号
+browser.find_element_by_xpath('//*[@id="password"]').send_keys(pwd)  # 填入你的密码
+browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div[3]/div/form/p[4]/button').click()
+time.sleep(10)
+
 print("3")
 try:
-    browser.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/div/div/div/div/form/div[9]/div/button").click()
+    browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/button[1]').click()
+    time.sleep(7)
+    browser.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/button').click()
+    time.sleep(5)
+    browser.find_element_by_xpath('/html/body/div[3]/div/div[3]/button[2]').click()
 except selenium.common.exceptions.NoSuchElementException:
     api = 'https://sc.ftqq.com/' + sckey + '.send'
-    title = "上报成功"
+    title = "今日已经上报过了"
     content = "今日已经上报过了！"
     data = {
         "text" : title,
