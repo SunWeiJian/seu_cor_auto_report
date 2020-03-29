7e556bbae370e948d693cd73418dfb5f73cc55a4
SunWeiJian <weijiansun@outlook.com>
2020-03-29 13:29:34 +0800

    适配github actions

--- app.py
+++ app.py
@@ -3,16 +3,33 @@ import time
 
 from selenium import webdriver
 import selenium
+import os
 
-opt = webdriver.ChromeOptions()  # 创建浏览器
-# opt.headless = True  # 无窗口模式 可注释此行查看chrome运行情况
-driver = webdriver.Chrome(options=opt)  # 创建浏览器对象
-driver.get('https://xgbxscwx.seu.edu.cn/#/')  # 打开网页
+try:
+    sckey = os.environ(sckey)
+try:
+    user = os.environ(username)
+    pwd = os.environ(password)
+except:
+    print("参数不完整或错误，请检查用户名、密码是否正确填写")
+    exit(1)
+
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-infobars')
+options.add_argument("--disable-extensions");
+options.add_argument("--disable-gpu");
+options.add_argument("--disable-dev-shm-usage");
+options.add_argument("--no-sandbox");
+options.add_argument("--headless");
+browser = webdriver.Chrome(options=options)
+browser.set_window_size(1200, 2000)
+
+browser.get('https://xgbxscwx.seu.edu.cn/#/')  # 打开网页
 time.sleep(3)
 
-driver.find_element_by_xpath("//input[@id='username']").send_keys("账号")  # 填入你的一卡通号
-driver.find_element_by_xpath("//input[@id='password']").send_keys("密码")  # 填入你的密码
-driver.find_element_by_xpath("//button[@type='submit']").click()
+browser.find_element_by_xpath("//input[@id='username']").send_keys(user)  # 填入你的一卡通号
+browser.find_element_by_xpath("//input[@id='password']").send_keys(pwd)  # 填入你的密码
+browser.find_element_by_xpath("//button[@type='submit']").click()
 time.sleep(2)
 
 print(driver.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/h2").text)
@@ -22,6 +39,24 @@ time.sleep(2)
 try:
     driver.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/div/div/div/div/form/div[9]/div/button").click()
 except selenium.common.exceptions.NoSuchElementException:
-    print('今日已上报')
+    api = 'https://sc.ftqq.com/' + sckey + '.send'
+    title = "上报成功"
+    content = "今日已经上报过了！"
+    data = {
+        "text" : title,
+        "desp" : content
+            }
+    req = requests.post(api, data = data)
+    print("推送成功，假如没有收到推送，请检查key是否正确")
+    exit(0)
 else:
-    print('上报成功！')
+    api = 'https://sc.ftqq.com/' + sckey + '.send'
+    title = "上报成功"
+    content = "今日上报成功！"
+    data = {
+        "text" : title,
+        "desp" : content
+            }
+    req = requests.post(api, data = data)
+    print("推送成功，假如没有收到推送，请检查key是否正确")
+    exit(0)
--- readme.md
+++ readme.md
@@ -1,18 +1,4 @@
 # 东南学工疫情信息自动上报
-### 使用说明
-（推荐使用pycharm打开）
-
-
-- 需要将\venv\Scripts\chromedriver.exe 替换为对应chrome版本的
-[chromedriver](https://chromedriver.chromium.org/downloads "chromedriver") 
-
-如果无法使用venv/pycharm：
-- 需要安装selenium（以及对应的chromedriver）
-
-（默认使用的是80版本的Chrome）
-
-在app.py中填入用户名和密码即可使用。
-当前版本暂不可用，仅供测试。
-DEV testgaile
-
+本人自用
+使用selenium 、github actions 并实现微信推送通知
 
